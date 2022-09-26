dcd76002b086810d881a01d4eefdada547c0ba2c
zlewchan <x@zlew.tf>
2022-09-26 19:32:17 +0200

    miatoll: Rebrand all SM6250 overlays to Miatoll
    Signed-off-by: zlewchan <x@zlew.tf>

--- device.mk
+++ device.mk
@@ -268,14 +268,14 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
-    SM6250Frameworks \
-    SM6250SystemUI \
+    MiatollFrameworks \
+    MiatollSystemUI \
     TelephonyResCommon \
     WifiOverlay
 
 PRODUCT_PACKAGES += \
-    SM6250LineageDialer \
-    SM6250LineageSDK
+    MiatollLineageDialer \
+    MiatollLineageSDK
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
